5d62069fc7f450fd02a0581b2b428c5f6026234b
herman <109037665+UHZ1@users.noreply.github.com>
2024-06-04 10:53:34 -0400

    Added main function for bitboard to run game, no bounds checking yet

---
+++ bitmain.py
@@ -0,0 +1,20 @@
+from bitboard import bitboard
+from mcts import task
+
+def play():
+  b = bitboard()
+  while b.is_terminal() == -2:
+    mv = int(input("Enter: "))
+    b.move(mv)
+    b.print()
+    if b.is_terminal() != -2:
+      break;
+    print()
+    mv = task(b, 3000, 150)
+    b.move(mv)
+    b.print()
+
+  print("Winner: " + str(b.winner))
+  return
+
+play()
